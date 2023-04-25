142ea547a8679199c73294e4b12de06bb10dc121
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-04-25 10:35:58 -0300

    aaaaaaaaaaaaaaaaa

--- gui.py
+++ gui.py
@@ -12,6 +12,53 @@ def derivada(x):
     return (math.pow(x, 2) - math.exp(x)) / h
 
 
+flag = True
+
+while flag:
+    print(f"Digite 1 para aproximar raiz por bissecção: ")
+    print(f"Digite 2 para aproximar raiz por falsa-posição: ")
+    print(f"Digite 3 para aproximar raiz por NewTon-Raphson: ")
+    print(f"Digite 4 para aproximar raiz por Iteração Linear: ")
+    print("QUALQUER TECLA - SAIR")
+
+    escolha = int(input("Digite:"))
+
+    if escolha == 1:
+        a = float(input("Digite valor de limite inicial (a): "))
+        b = float(input("Digite o valor de limite final (b): "))
+        limite = float(input("Digite o erro: "))
+
+        method.bissection(a, b , limite)
+        
+
+    elif escolha  == 2:
+        a = float(input("Digite valor de limite inicial (a): "))
+        b = float(input("Digite o valor de limite final (b): "))
+        limite = float(input("Digite o erro: "))
+
+        method.fakePos(a ,b , limite)
+
+    elif escolha  == 3:
+        chuteInicial = float(input("Digite o chute inicial: "))
+        limite = float(input("Digite o erro: "))
+
+        method.newtonRaphson(chuteInicial, limite)
+    
+    elif escolha  == 4:
+        chuteInicial = float(input("Digite o chute inicial: "))
+        limite = float(input("Digite o erro: "))
+
+        method.fixedPoint(chuteInicial, limite)
+
+    else:
+        flag = False
+
+print("Acabou: ")
+
+
+        
+
+        
 
 
 
